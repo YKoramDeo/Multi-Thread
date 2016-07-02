3122868bdc7b12425e19c43776b34f94b6f3d2c4
이근용 <ky9230@gmail.com>
2016-07-03 02:52:24 +0900

    Practice#01 : HelloWorld.cpp 프로그램을 작성하고 컴파일 후 실행하시오. 구현 완료

---
+++ source_code/Practice01_HelloWorld.cpp
@@ -0,0 +1,15 @@
+/*
+*
+*	Practice #01
+*
+* "Hello World!" 라는 문장을 출력하는 프로그램을 
+* 작성하고 컴파일 후 실행하시오
+*
+*/
+
+#include <iostream>
+int main(int argc, char *argv[])
+{
+	std::cout << "Hello World!" << std::endl;
+	return 0;
+}
